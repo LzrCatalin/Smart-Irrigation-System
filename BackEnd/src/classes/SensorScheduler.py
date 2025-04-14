39e0ae098ec0dad3aea95f4486e50b44f261a88a
LzrCatalin <lazar.florin.catalin02@gmail.com>
2025-04-14 18:43:02 +0300

    Change logic implementation. Now for each field update sensors that are available.

--- BackEnd/src/classes/SensorScheduler.py
+++ BackEnd/src/classes/SensorScheduler.py
@@ -2,6 +2,7 @@ import atexit
 from colorama import Fore
 from flask_apscheduler import APScheduler
 from src.services import sensors_services
+from src.services import fields_service
 from src.classes.Sensor import *
 from src.classes.Field import *
 from src.sensors.humidity_sensor import *
@@ -14,148 +15,86 @@ from src.services import fields_service
 #
 ####################
 class SensorScheduler:
-	def __init__(self, app):
+	def __init__(self, app, irrigation_system):
 		self.scheduler = APScheduler()
 		self.scheduler.init_app(app)
 		self.scheduler.start()
+		self.irrigation_system = irrigation_system
+
+	def periodic_fields_sensor_update(self):
+		"""Run periodic fields measurements"""
 		
+		logging.debug(Fore.MAGENTA +
+				"\t <!> Starting Periodic Update <!>"
+				+ Style.RESET_ALL)
 
-	def periodic_sensor_update(self):
-			logging.debug(Fore.MAGENTA +
-					"\t <!> Starting Periodic Update <!>"
-					+ Style.RESET_ALL)
-			
-			# Fetch all ids from Firebase
-			sensor_ids = sensors_services.get_sensors_ids()				
+		# Fetch fields ids
+		fields_data = fields_service.get_fields_data()
 
-			# Start updating if exists any ids in db
-			if sensor_ids:
+		for field_id, field_data in fields_data.items():
+			if not field_data or not field_data['sensors']:
+				continue
 
-				# Iterate through sensors ids
-				for sensor_id in sensor_ids:
-					# Retrieve sensor data based on id
-					sensor_data = sensors_services.get_sensor_data_by_id(sensor_id)
-					
-					# Fetch sensor port , type and status
-					sensor_port = sensor_data['type']['port']
-					sensor_type = sensor_data['type']['type']
-					sensor_status = sensor_data['type']['status']
+			# Initialize measurements
+			new_humidity = None
+			new_temperature = None
 
-					# Display details
-					self.display_sensor_details(sensor_id, sensor_data)
+			# Update field's sensors
+			for sensor in field_data['sensors']:
+				
+				# Display each sensor data
+				self.display_sensor_details(sensor)
 
-					#
-					#	Updating only sensors that are displayed on the fields
-					#
-					if sensor_status == Status.AVAILABLE.name:
-						logging.info(Fore.CYAN +
-								f"\t\tSensor with id: {id} not in function." 
-									+ Style.RESET_ALL)
+				sensor_type = sensor['type']['type']
+				sensor_port = sensor['type']['port']
 
-						continue
-					
-					# Fetch adc_value based on port
-					if sensor_port >= 0:
-						logging.debug(Fore.MAGENTA +
-							f"Start for port: {sensor_port}"
-							+ Style.RESET_ALL)
+				if sensor_type == Type.HUMIDITY.name:
+					adc_value = sensor_setup(sensor_port)
 
-						# Check sensor type
-						if sensor_type == Type.HUMIDITY.name:
-							adc_value = sensor_setup(sensor_port)
+					if adc_value is not None:
+						new_humidity = moisture_percentage(adc_value)
+						sensor['type']['measured_value'] = new_humidity
 
-							# Set new measured value
-							if adc_value is not None:
-								sensor_data['type']['measured_value'] = moisture_percentage(adc_value)
+				elif sensor_type == Type.TEMPERATURE.name:
+					slave_file = sensor_file(sensor_port)
 
-								# Call update function
-								self.update(sensor_id, sensor_data, adc_value)
+					if slave_file is not None:
+						new_temperature = read_temperature(slave_file)
+						sensor['type']['measured_value'] = new_temperature
 
-						elif sensor_type == Type.TEMPERATURE.name:
-							# Search slave file for sensor port
-							slave_file = sensor_file(sensor_port)
+			# Update field entity in the database
+			if new_humidity is not None and new_temperature is not None:
+				fields_service.update_field_measurements_by_id(field_id, Field.from_dict(field_data))
 
-							# Check if slave file exists and set new measured value
-							if slave_file is not None:
-								sensor_data['type']['measured_value'] = read_temperature(slave_file)
+				# Update irrigation system management
+				self.irrigation_system.add_field(field_id)
+				self.irrigation_system.update_field_measurements(
+					field_id= field_id,
+					humidity=new_humidity if new_humidity is not None 
+										else self.irrigation_system.fields[field_id].current_humidity,
+					temperature=new_temperature if new_temperature is not None 
+										else self.irrigation_system.fields[field_id].current_temperature					
+										)
 
-								# Call update function
-								self.update(sensor_id, sensor_data, slave_file)
-
-						else:
-							# Handle unknown type
-							logging.info(Fore.WHITE + 
-									f"Type: {type} unknown." +
-										Style.RESET_ALL)
-							break
-
-			else:
-				# No ids
-				logging.info(Fore.LIGHTYELLOW_EX +
-						f'No sensors in database.\n\tNo incoming updates.\n' +
-						Style.RESET_ALL)
-
-
-
-	def schedule_sensor_updates(self, duration):
-		print(f"Sensor scheduler Turn ON for every {duration} seconds.")
-		self.scheduler.add_job(id='sensor_update', 
-						func=self.periodic_sensor_update,
-						trigger = 'interval', 
-						seconds = duration)
-
+	def schedule_sensor_updates(self, interval: int):
+		"""Schedule automatic updates on sensors cycle"""
+		if not self.scheduler.get_job('sensor_update_cycle'):
+			self.scheduler.add_job(
+				id='sensor_update_cycle', 
+				func=self.periodic_fields_sensor_update,
+				trigger = 'interval', 
+				seconds = interval
+			)
+			logging.info(f'Scheduled sensors updates cycles every {interval} seconds')
 
 	def scheduler_shutdown(self):
 		print("Sensors scheduler shutdown.")
 		atexit.register(lambda: self.scheduler_shutdown())
 
-
 	def get_schedules(self):
 		print(self.scheduler.get_jobs())
 
-
-	def update(self, sensor_id: str, sensor_data: dict, value: float | str) -> None:
-		# Convert sensor_data to Sensor object
-		updated_sensor = Sensor(
-			name=sensor_data["name"],
-			type=SensorType(
-				type=sensor_data["type"]["type"],
-				measured_value=sensor_data["type"]["measured_value"],
-				status=sensor_data["type"]["status"],
-				port=sensor_data["type"]["port"],
-			),
-			field_id=sensor_data.get("field_id")
-		)
-
-
-		# Retrieve field id from sensor data
-		field_id = updated_sensor.field_id
-
-		if field_id:
-			# Fetch field based on id
-			field = fields_service.get_field_by_id(field_id)
-
-			if field:
-				# Update the sensor in the field
-				for sensor in field["sensors"]:
-					if sensor["name"] == updated_sensor.name and sensor['type']['type'] == Type.HUMIDITY.name:
-						sensor["type"]["measured_value"] = moisture_percentage(value)
-						break
-
-					if sensor["name"] == updated_sensor.name and sensor['type']['type'] == Type.TEMPERATURE.name:
-						sensor["type"]["measured_value"] = read_temperature(value)
-						break
-
-		# Insert new data into Firebase
-		sensors_services.update_sensor_by_id(sensor_id, updated_sensor)
-		fields_service.update_field_measurements_by_id(field_id, Field.from_dict(field))
-
-
-	def display_sensor_details(self, sensor_id: str, sensor_data: dict) -> None:
-		logging.debug(Fore.MAGENTA +
-		f"ID: {sensor_id} ---> Data: {sensor_data}"
-		+ Style.RESET_ALL)
-		
+	def display_sensor_details(self, sensor_data: dict) -> None:
 		logging.debug(Fore.MAGENTA +
 			f"Port: {sensor_data['type']['port']}"
 			+ Style.RESET_ALL)
