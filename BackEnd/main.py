1adc92079166e9b392e78ffdc4eae06c612988a7
Catalin Lazar <lazar.florin.catalin02@gmail.com>
2025-05-15 01:16:35 +0300

    Rearanged main

--- BackEnd/main.py
+++ BackEnd/main.py
@@ -5,16 +5,13 @@ from flask_apscheduler import APScheduler
 from flask_cors import CORS
 
 from src.classes.OAuthManager import OAuthManager
-#from src.classes.SensorScheduler import SensorScheduler
-#from src.sensors.humidity_sensor import *
 
-from src.controllers.sensors_controller import *
-from src.controllers.users_controller import *
-from src.controllers.fields_controller import *
-from src.controllers.oauth_controller import *
+from src.controllers.sensors_controller import sensors_bp
+from src.controllers.users_controller import users_bp
+from src.controllers.fields_controller import fields_bp
+from src.controllers.oauth_controller import auth_bp
+from src.controllers.alerts_controller import alerts_bp
 
-# from src.services.sensors_services import *
-# from src.firebase.sensors_init import sensors_init
 ####################
 #
 #   Add the path to the project
@@ -54,12 +51,6 @@ class Config:
 
 app.config.from_object(Config())
 
-# Init sensor scheduler
-# sensors_scheduler = SensorScheduler(app)
-
-# Start periodic update at a 30 seconds interval
-# sensors_scheduler.schedule_sensor_updates(5)
-
 ####################
 #
 #   Blueprints Initializations
@@ -70,6 +61,7 @@ app.register_blueprint(sensors_bp)
 app.register_blueprint(users_bp)
 app.register_blueprint(fields_bp)
 app.register_blueprint(auth_bp)
+app.register_blueprint(alerts_bp)
 
 if __name__ == '__main__':
 	app.run(debug=False, host = "0.0.0.0", port = 5000)
