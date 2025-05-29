14a620079f7d1893a639444fdb15deba2a266022
Catalin Lazar <lazar.florin.catalin02@gmail.com>
2025-05-29 23:53:04 +0300

    Implemented new function to fetch location of a field using id

--- BackEnd/src/api/geocodinAPI.py
+++ BackEnd/src/api/geocodinAPI.py
@@ -1,7 +1,18 @@
 from dotenv import load_dotenv
 import os, requests
+
+from src.services.fields_service import get_field_by_id
+
 # Load .env file
 load_dotenv()
+
+def get_location_by_field_id(field_id: str) -> str:
+
+	# Verify field id
+	field_data = get_field_by_id(field_id)
+
+	# Return the location
+	return (get_location(field_data['latitude'], field_data['longitude']))
 
 def get_location(latitude: float, longitude: float) -> str:
 	# Fetch key from .env file
