f0326477e5d099bcc08c3dd4cdf4925c40ff4324
Catalin Lazar <lazar.florin.catalin02@gmail.com>
2025-06-05 15:16:23 +0300

    Little change :D

--- FrontEnd/src/app/app.component.ts
+++ FrontEnd/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'FrontEnd';
+
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    this.loadGoogleMaps();
+  }
+
+  loadGoogleMaps(): void {
+    const script = document.createElement('script');
+    script.src = `https://maps.googleapis.com/maps/api/js?key=${environment.googleMapsApiKey}&callback=initMap`;
+    script.async = true;
+    script.defer = true;
+    document.body.appendChild(script);
+  }
 }
